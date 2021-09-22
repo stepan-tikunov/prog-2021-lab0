e7c7b71103b0f7fa887904eda0510f9baf208528
googolplex <gglplxxx@gmail.com>
2021-09-22 20:52:53 +0300

    Fix compile.sh

--- compile.sh
+++ compile.sh
@@ -3,5 +3,6 @@
 cd "$(dirname "$0")";
 mkdir bin 2>/dev/null;
 javac Main.java -d bin/;
-jar cfm bin/lab0.jar MANIFEST.mf bin/Main.class;
+cd bin;
+jar cfm lab0.jar ../MANIFEST.mf Main.class;
 echo "Compiled successfully, lab0.jar and Main.class are stored in bin/directory.";
