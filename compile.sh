f9423cd05c519f56f79cc20d2638410f56c691a5
googolplex <gglplxxx@gmail.com>
2021-09-22 23:20:11 +0300

    Fix compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd "$(dirname "$0")";
-mkdir bin 2>/dev/null;
-javac Main.java -d bin/;
-cd bin;
-jar cfm lab0.jar ../MANIFEST.mf Main.class;
-echo "Compiled successfully, lab0.jar and Main.class are stored in bin/directory.";
+cd "$(dirname "$0")" ;
+
+mkdir bin 2>/dev/null ;
+javac Main.java -d bin/ >/dev/null &&
+cd bin &&
+jar cfm lab0.jar ../MANIFEST.mf Main.class >/dev/null &&
+echo "Compiled successfully, lab0.jar and Main.class are stored in bin/directory." ;
