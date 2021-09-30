23bfcfcf722082985d526896ace68c0c9e52a4e2
googolplex <gglplxxx@gmail.com>
2021-09-30 10:55:22 +0300

    Align numbers to columns

--- src/edu/ifmo/tikunov/lab0/Main.java
+++ src/edu/ifmo/tikunov/lab0/Main.java
@@ -54,7 +54,7 @@ public class Main {
 
 		for (int i = 0; i < SIZE; ++i) {
 			for (int j = 0; j < SIZE; ++j) {
-				System.out.printf("%15.5f ", y[i][j]);
+				System.out.printf("%12.5f ", y[i][j]);
 			}
 			System.out.println();
 		}
