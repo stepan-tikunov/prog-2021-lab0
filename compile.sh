31824b12d59bedfa2cb8d57d83be37bd2febf768
Stepan Tikunov <stepan.tikunov@niuitmo.ru>
2021-09-30 10:47:24 +0300

    Modify compile.sh

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")" ;
 
 rm bin/app.jar $(find bin -name "*.class") 2>/dev/null ;
 mkdir bin 2>/dev/null ;
-javac -s src/ -d bin/ $(find src -name "*.java") &&
+javac -g -s src/ -d bin/ $(find src -name "*.java") &&
 jar cfm bin/app.jar MANIFEST.mf -C bin/ $(cd bin/ && find . -name "*.class") &&
 echo "Compiled successfully, app.jar and *.class files are stored in bin/ directory." ;
